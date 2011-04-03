ec5f9cd73f4803703d55ee4a37ba4abdee3fef9b
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2011-04-03 22:24:20 +0200

    Game logic, excluding swaps, implemented. Untested.

--- scrabble.py
+++ scrabble.py
@@ -1,5 +1,6 @@
-import random
+from counter import Counter
 
+import random
 
 class Bag(object):
     letter_distribution = {
@@ -49,48 +50,202 @@ class Bag(object):
         return cls.letter_distribution[tile][1]
 
 
-class Board(object):
-    word_multiplier = None
-    letter_multiplier = None
+def get_multipliers():
+    if hasattr('multipliers', get_multipliers):
+        return get_multipliers.multipliers
 
-    def __init__(self):
-        if Board.word_multiplier is None:
-            Board.calculate_multipliers()
-        self.board = [[None] * 15 for i in range(15)]
+    board_layout = """
+3w    2l      3w      2l    3w
+  2w      3l      3l      2w
+    2w      2l  2l      2w
+2l    2w      2l      2w    2l
+        2w          2w
+  3l      3l      3l      3l
+    2l      2l  2l      2l
+3w    2l      2w      2l    3w
+    2l      2l  2l      2l
+  3l      3l      3l      3l
+        2w          2w
+2l    2w      2l      2w    2l
+    2w      2l  2l      2w
+  2w      3l      3l      2w
+3w    2l      3w      2l    3w
+"""
 
-    @classmethod
-    def calculate_multipliers(cls):
-        cls.word_multiplier = {}
-        cls.letter_multiplier = {}
+    word = {}
+    letter = {}
+    rows = board_layout.split('\n')[1:-1]
+    for r, row in enumerate(rows):
+        for c in range(15):
+            if c * 2 + 1 >= len(row) or row[c * 2 + 1] == ' ':
+                continue
+            elif row[c * 2 + 1] == 'w':
+                word[(r, c)] = int(row[c * 2])
+            else:
+                assert row[c * 2 + 1] == 'l', row[c * 2 + 1]
+                letter[(r, c)] = int(row[c * 2])
 
-        for r in (0, 7, 14):
-            for c in (0, 7, 14):
-                cls.word_multiplier[(r, c)] = 3
-        cls.word_multiplier[(7, 7)] = 2
-        for i in range(1, 5):
-            cls.word_multiplier[(i, i)] = 2
-            cls.word_multiplier[(i, 14 - i)] = 2
-            cls.word_multiplier[(14 - i, i)] = 2
-            cls.word_multiplier[(14 - i, 14 - i)] = 2
+    get_multipliers.multipliers = {'word': word, 'letter':letter}
+    return get_multipliers.multipliers
 
-        for dr, dc in ((5, 1), (1, 5), (5, 5)):
-            cls.letter_multiplier[(dr, dc)] = 3
-            cls.letter_multiplier[(14 - dr, dc)] = 3
-            cls.letter_multiplier[(dr, 14 - dc)] = 3
-            cls.letter_multiplier[(14 - dr, 14 - dc)] = 3
 
-        for dr, dc in ((3, 0), (6, 2), (0, 3), (7, 3), (2, 6), (6, 6), (3, 7)):
-            cls.letter_multiplier[(dr, dc)] = 2
-            cls.letter_multiplier[(14 - dr, dc)] = 2
-            cls.letter_multiplier[(dr, 14 - dc)] = 2
-            cls.letter_multiplier[(14 - dr, 14 - dc)] = 2
+def get_dictionary(dict_name):
+    return set()
 
+class IllegalMove(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return 'IllegalMove: ' + self.message
 
 class ScrabbleGame(object):
-    def __init__(self, num_players):
+    def __init__(self, num_players, dict_name='SOWPODS'):
         assert 2 <= num_players <= 4
+        self.num_players = num_players
         self.bag = Bag()
-        self.racks = [self.bag.pop_tiles(7) for i in range(num_players)]
+        self.racks = [Counter(self.bag.pop_tiles(7) for i in range(num_players))]
+        self.scores = [0 for i in range(num_players)]
+        self.player = 0
+        self.board = {}
+        self.dictionary = get_dictionary(dict_name)
+        self.winner = []
+        self.scoreless_turns = 0
 
+        multipliers = get_multipliers()
+        self.word_mul = multipliers['word']
+        self.letter_mul = multipliers['letter']
 
-Board.calculate_multipliers()
+    def play_tiles(self, played_tiles):
+        if not played_tiles:
+            self.scoreless_turn()
+            return ({}, 0)
+
+        # Work out new rack. Still needs to take tiles out of bag once we know
+        # the move played is valued.
+        old_rack = self.racks[self.player]
+        new_rack = old_rack - Counter(v if v.isupper() else '_'
+                                      for v in played_tiles.values())
+        old_rack_len = sum(old_rack.values())
+        new_rack_len = sum(new_rack.values())
+        if old_rack_len - len(played_tiles) != new_rack_len:
+            raise IllegalMove('Played tiles not in rack.')
+
+        # Check if each tile is in a valid position
+        for v in played_tiles.keys():
+            if v in self.board:
+                raise IllegalMove(
+                    'Position (%d, %d) already has a tile on it.' % v)
+            if not (0 <= v[0] < 15 and 0 <= v[1] < 15):
+                raise IllegalMove(
+                    'Tile at position (%d, %d) is not on the board.' % v)
+
+        # Check all the tiles are in a single row or a single column
+        if len(played_tiles) > 1:
+            x, y = list(played_tiles.keys())[:2]
+            for r, c in played_tiles:
+                horiz = r == x[0] == y[0]
+                vert  = c == x[1] == y[1]
+                if not (vert or horiz):
+                    raise IllegalMove(
+                        'Tiles need to align horizontally or vertically')
+
+        # Check if the tiles are connected
+        if len(played_tiles) > 1:
+            p = played_tiles.keys()
+            p.sort()
+            for i in range(len(p) - 1):
+                x, y = p[i], p[i + 1]
+                for j in range(1, y[0] - x[0] + y[1] - x[1]):
+                    if x[0] == y[0]:
+                        has_tile = (x[0], x[1] + j) in self.board
+                    else:
+                        has_tile = (x[0] + j, x[1]) in self.board
+                    if not has_tile:
+                        raise IllegalMove('All tiles need to be adjacent or '
+                                          'have already played tiles '
+                                          'inbetween them.')
+
+        # Get list of played words
+        words = {}
+        def get_word(r, c, dr, dc):
+            # Find start of word
+            while (r, c) in self.board or (r, c) in played_tiles:
+                r -= dr
+                c -= dc
+            r += dr
+            c += dc
+
+            # Calculate score
+            sr, sc = r, c
+            word_mul = 1
+            word = []
+            score = 0
+            while (r, c) in self.board or (r, c) in played_tiles:
+                pos = (r, c)
+                if pos in self.board:
+                    c = self.board[pos]
+                    word.append(c)
+                    score += Bag.score(c)
+                else:
+                    c = played_tiles[pos]
+                    word.append(c)
+                    v = Bag.score(c)
+                    if pos in self.word_mul:
+                        word_mul *= self.word_mul[pos]
+                    elif pos in self.letter_mul:
+                        v *= self.letter_mul[pos]
+                    score += v
+                r += dr
+                c += dc
+            score *= word_mul
+
+            return (''.join(word), (sr, sc), score)
+        # XXX a lot slower than necessary
+        for r, c in played_tiles:
+            word, start_pos, score = get_word(r, c, 1, 0)
+            words[(word, start_pos)] = score
+            word, start_pos, score = get_word(r, c, 0, 1)
+            words[(word, start_pos)] = score
+
+        for word, _ in words:
+            if word.lower() not in self.dictionary:
+                raise IllegalMove('"%s" is not a word.' % word)
+
+        # From here on out we know the move played is valid
+        score = sum(words.values())
+
+        # Update the rack
+        tiles_needed = 7 - new_rack_len
+        new_rack.update(self.bag.pop_tiles(tiles_needed))
+        self.racks[self.player] = new_rack
+
+        # Check for endgame
+        if not new_rack:
+            for i in range(self.num_players):
+                rack_value = sum(Bag.score(c) for c in self.rack.elements())
+                self.scores[i] -= rack_value
+                score += rack_value
+
+        self.scores[self.player] += score
+        self.scoreless_turns = 0
+
+        # End game now that the scores are updated
+        if not new_rack:
+            self.end_game()
+
+        self.player = (self.player + 1) % self.num_players
+
+        return (words, score)
+
+    def scoreless_turn(self):
+        self.player = (self.player + 1) % self.num_players
+        assert 0 <= self.scoreless_turns < 6
+        if any(self.scores):
+            self.scoreless_turns += 1
+            if self.scoreless_turns == 6:
+                self.end_game()
+
+    def end_game(self):
+        winning_score = max(self.scores)
+        self.winners = [i for i, score in enumerate(self.scores)
+                        if score == winning_score]
