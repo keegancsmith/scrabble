5fa2d782cd9a32554f5b674c2c838b822dda0ebb
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2011-05-26 23:59:58 +0200

    Added some config files for deployment.

---
+++ data/gunicorn_conf.py
@@ -0,0 +1,17 @@
+import multiprocessing
+import os.path
+
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+run_dir = os.path.join(os.path.dirname(__dir__), 'run')
+
+if not os.path.exists(run_dir):
+    import os
+    os.mkdir(run_dir)
+
+bind = 'unix:' + os.path.join(run_dir, 'gunicorn.sock')
+logfile = os.path.join(run_dir, 'gunicorn.log')
+pidfile = os.path.join(run_dir, 'gunicorn.pid')
+daemon = True
+
+work_class = 'gunicornsupport.EventletWorker'
+workers = multiprocessing.cpu_count() * 2 + 1
