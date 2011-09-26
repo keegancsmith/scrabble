b945b46425a3a2fb09cb64c4316468c613ad72c1
Max Rabkin <max.rabkin@gmail.com>
2011-09-26 21:11:08 +0200

    add active-games/json/
    --HG--
    branch : click-to-type

--- scrabbleapp/urls.py
+++ scrabbleapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns(
     (r'^$', 'active_games'),
     url(r'^create-game/$', 'create_game', name='game-create'),
     url(r'^active-games/$', 'active_games', name='list-active-games'),
-
+    url(r'^active-games/json/$', 'active_games_json', name='active-games-json'),
     url(r'^game/(\d+)/$', 'get_game', name='game'),
     url(r'^game/(\d+)/state/$', 'game_state', name='game-state'),
     url(r'^game/(\d+)/immutable-state/$', 'game_immutable_state', name='game-immutable-state'),
